ae16b8f9367d0289de55a1c25bbb9e89c795505b
Joanna Rosłaniec-Ryżyńska <58250058+JoannaRoslaniec354@users.noreply.github.com>
2023-05-19 16:23:21 +0200

    finish end-to-end tests with robust changes

--- src/helpers/database_helper.py
+++ src/helpers/database_helper.py
@@ -1,8 +1,12 @@
 import pymysql
 
+from test_framework.src.helpers.config_helpers import get_database_credentials
+from test_framework.src.configs.generic_configs import GenericConfigs
+
 
 def read_from_db(sql):
-    connection = pymysql.connect(host='127.0.0.1', port=8889, user='root', password='root')
+    db_creds = get_database_credentials()
+    connection = pymysql.connect(host=db_creds['db_host'], port=db_creds['db_port'], user=db_creds['db_user'], password=db_creds['db_password'])
     try:
         cursor_item = connection.cursor(pymysql.cursors.DictCursor)
         cursor_item.execute(sql)
@@ -14,7 +18,9 @@ def read_from_db(sql):
 
 
 def get_order_from_db_by_order_no(order_no):
-    sql = f"SELECT * FROM quicksitedb.wp_posts WHERE ID={order_no} AND post_type='shop_order';"
+    schema = GenericConfigs.DATABASE_SCHEMA
+    table = GenericConfigs.DATABASE_TABLE_ORDERS
+    sql = f"SELECT * FROM {schema}.{table} WHERE ID={order_no} AND post_type='shop_order';"
 
     db_order = read_from_db(sql)
     return db_order
